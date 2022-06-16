2f5b636783b53e9b2f3bc44ed8cae8825d283bc3
drunkcanary <caghedo13@gmail.com>
2022-06-16 23:53:47 +0000

    added some htmls

--- application/routes.py
+++ application/routes.py
@@ -1,14 +1,25 @@
 from application import app, db
-from application.models import ToDos
+from application.models import Todos
+from flask import redirect, url_for, render_template
 
 
 
 
 @app.route('/')
 def index():
-    todo=ToDos.query.first()
-    return f'{todo.task}+{todo.completed}'
+    todo=Todos.query.first()
+    return f'{todo.task}{todo.completed}'
 
-@app.route('/done')
+@app.route('/alltasks')
 def done():
-    return "Task completed"
+    todo=Todos.query.all()
+    empstr = ""    
+    for t in todo:
+        empstr += f'{t.id} {t.task}  {t.completed} <br>'    
+    return empstr
+
+
+@app.route('/firsttask')
+def htmldone():
+    todo=Todos.query.all()
+    return render_template("about.html",todos=todo)
--- create.py
+++ create.py
@@ -1,4 +1,18 @@
 from application import db
+from application.models import Todos
 
 db.drop_all()
 db.create_all()
+sample_todo=Todos(
+    task="Sample todo",
+    completed=False
+)
+second_todo=Todos(
+    task="do homework for christian", 
+    completed=False)
+third_todo=Todos(task="beat the fuck out of him", completed=True)
+
+db.session.add(sample_todo)
+db.session.add(second_todo)
+db.session.add(third_todo)
+db.session.commit()
